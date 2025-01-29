758ebaf4cf077cd6c322c8f7d5ba4ad455def956
Alejandro Salas Barceló <devalexops@gmail.com>
2025-01-29 01:34:43 +0100

    Added CM and modified some typos on IaC and variables

--- IaC/variables.tf
+++ IaC/variables.tf
@@ -1,19 +1,16 @@
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr" {
   description = "The CIDR block for the subnet"
   type        = string
-  default     = "10.20.1.0/24"
 }
 
 variable "availability_zone" {
   description = "The availability zone for the subnet"
   type        = string
-  default     = "us-west-3a"
 }
 
 variable "route_cidr_block" {
@@ -43,19 +40,16 @@ variable "ssh_port" {
 variable "ami_id" {
   description = "The AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-06e02ae7bdac6b938"
 }
 
 variable "jenkins_sku" {
   description = "The instance type for the Jenkins instance"
   type        = string
-  default     = "t2.medium"
 }
 
 variable "worker_sku" {
   description = "The instance type for the worker instance"
   type        = string
-  default     = "t2.micro"
 }
 
 variable "key_name" {
